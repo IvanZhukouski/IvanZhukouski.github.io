aac7c0477786eaf78288a8c39742ae63ffdcf498
IvanZhukouski <ivan.zhukouski@mail.ru>
2018-03-29 18:16:52 +0300

    +переключение картинки по клику в галерее

--- script.js
+++ script.js
@@ -3,20 +3,37 @@ $(document).ready(function(){
     $(".menu").click(function(){
         $('.menu-links').slideToggle();
         $(this).toggleClass('menu-open');
-        //$(this).toggleClass('menu')
     });
     $("main").click(function(){
         $('.menu-links').slideUp();
         $('.menu').removeClass('menu-open');
-        //$(this).toggleClass('menu')
     });
+
+
     $('.gallery-photo img').click(function(){
     	$('.photo-large').slideDown();
     	var img = $(this);
+        $(img).parent().addClass('currentImage')
 		var src = img.attr('src');
 		$('.photo-large').css('background-image','url(' + src + ')');
     })
+    var nextImgSrc = 1;
     $('.photo-large').click(function(){
-    	$(this).hide();
+        
+        var nextImgDiv = $('.currentImage  + div');
+        $('.currentImage').removeClass('currentImage');
+        $(nextImgDiv).addClass('currentImage')
+        var nextImg = $(nextImgDiv).children();
+        nextImgSrc = nextImg.attr('src');
+        if(nextImgSrc){
+        $('.photo-large').css('background-image','url(' + nextImgSrc + ')');
+        }
+        else{
+            //nextImgDiv = $('.currentImage  + div');
+            $('.photo-large').slideUp();
+        }
+     })
+    $('.closeGallery').click(function(){
+        $('.photo-large').slideUp();
     })
 });
